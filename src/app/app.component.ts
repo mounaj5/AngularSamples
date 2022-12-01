3c17fca58f24d9f3047567b903e7cffdc9ece1d7
A Mouna <MAA10@dsone.3ds.com>
2022-12-01 19:17:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PasswordGenerator';
+  title = 'Password Generator';
+  letters = 'abcdefghijklmnopqrstuvwxyz';
+  numbers = '0123456789';
+  symbols = '!@#$%^&*()';
+  password='';
+  searchString = '';
+  useLetters:boolean = false;
+  useNumbers:boolean = false;
+  useSymbols:boolean = false;
+  length:number = 12;
+
+  changeUseLetters()
+  {
+  this.useLetters=!this.useLetters;
+  this.updateSearchString();
+  console.log("letters");
+  }
+
+  changeUseNumbers()
+  {
+  this.useNumbers=!this.useNumbers;
+  this.updateSearchString();
+  console.log("numbers");
+  }
+
+  changeUseSymbols()
+  {
+  this.useSymbols=!this.useSymbols;
+  this.updateSearchString();
+  console.log("symbols");
+  }
+
+  updateSearchString()
+  {
+  this.searchString='';
+  if(this.useLetters)
+    this.searchString+=this.letters;
+  if(this.useNumbers)
+    this.searchString+=this.numbers;
+  if(this.useSymbols)
+    this.searchString+=this.symbols;
+  }
+
+  generatePassword()
+  {
+  this.password='';
+  for(let i=0;i<=this.length;i++)
+  {
+    let j = Math.floor(Math.random() * this.searchString.length);
+    this.password += this.searchString[j];
+  }
+  }
+
 }
